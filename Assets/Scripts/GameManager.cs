d8a77e995b6fc4f2fe227a3d18f08149871daf75
kkumovic <kristijankumovic.kiki@gmail.com>
2023-09-25 21:14:31 +0200

    Revert "123"
    This reverts commit fb3b2acb683aa13f5593f459be240ece1bd6d2e5.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,15 +7,7 @@ public class GameManager : MonoBehaviour
     public static GameManager Instance;
     private void Awake()
     {
-        if (Instance != null)
-        {
-            Destroy(gameObject);
-        }
-        else
-        {
-            Instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
+        Instance = this;
     }
     private void OnEnable()
     {
