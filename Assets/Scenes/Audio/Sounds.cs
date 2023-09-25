f4c31b7c2edde65fe982b64639fb63efb99d2cb7
ZipiZipi <veljkoned@gmail.com>
2023-09-25 18:12:23 +0200

    Revert "Sounds Script Change"
    This reverts commit 0cc416b7b023eb77eafdbafb7c4df1cef8a35290.

--- Assets/Scenes/Audio/Sounds.cs
+++ Assets/Scenes/Audio/Sounds.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [System.Serializable]
-public class Sounds
+public class Sounds : MonoBehaviour
 {
     public string Name;
     public AudioClip Clip;
